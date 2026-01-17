4a313ac15b948afc3fd55a9ada084d708baeabf0
tnavya55 <234g1a05b1@srit.ac.in>
2026-01-17 15:12:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "personal.html"));
+});
+
+app.get("/personal", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "personal.html"));
+});
+
+app.get("/education", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "education.html"));
+});
+
+app.get("/project", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "project.html"));
+});
+
+app.get("/experience", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "experience.html"));
+});
+
+app.get("/hobbies", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "hobbies.html"));
+});
+
+app.get("/others", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "others.html"));
+});
+app.listen(PORT, () => {
+  console.log("Server running at http://localhost:3000");
+});
